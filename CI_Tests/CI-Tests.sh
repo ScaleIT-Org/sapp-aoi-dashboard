e301c6623a638dd8c8d8e3427d2287516549aaa6
Nico Kuhn <kuhn@fzi.de>
2017-09-26 04:23:07 +0200

    las

--- CI_Tests/CI-Tests.sh
+++ CI_Tests/CI-Tests.sh
@@ -1,4 +1,4 @@
-if curl dashboard:3000 | grep -q '<title>SICK AOI Data Systems</title>'; then
+if curl dashboard:3000 | grep -q '<title>SICK AOI Data Systems2</title>'; then
   echo "Tests passed!"
   exit 0
 else
