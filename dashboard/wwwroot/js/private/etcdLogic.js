17e727bb4d993e01d4e0b9197f8bef028a903d1a
Nico Kuhn <kuhn@teco.edu>
2017-11-18 22:25:48 +0100

    Fixed cross origin policy of etcd

--- dashboard/wwwroot/js/private/etcdLogic.js
+++ dashboard/wwwroot/js/private/etcdLogic.js
@@ -1,6 +1,6 @@
 //curl http://127.0.0.1:2379/v2/keys/Apps
 var xhr = new XMLHttpRequest();
-xhr.open('GET', "//127.0.0.1:2379/v2/keys/Apps", true);
+xhr.open('GET', "http://192.168.0.29:2379/v2/keys/Apps", true);
 xhr.send();
 xhr.onreadystatechange = processRequest;
 
