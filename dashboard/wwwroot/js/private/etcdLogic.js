c842af1f466383b739bfe58834d8e920c58e1680
Nico Kuhn <kuhn@teco.edu>
2017-11-19 15:01:00 +0100

    Added machine unsubscibe functionality to sidebar

--- dashboard/wwwroot/js/private/etcdLogic.js
+++ dashboard/wwwroot/js/private/etcdLogic.js
@@ -9,6 +9,10 @@ var sidebarMap = new Map();
 function processRequest(e) {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var response = JSON.parse(xhr.responseText);
+        sidebarMap.forEach(function(value, key, map) {
+            sidebarMap.set(key, 0);
+        });
+
         for (i in response.node.nodes)
         {
             if(!sidebarMap.has(response.node.nodes[i].key)){
@@ -16,6 +20,7 @@ function processRequest(e) {
                 //Add Sidebar Element
                 var ul = document.getElementById("sidebar-ul");
                 var li = document.createElement("li");
+                li.setAttribute("id",response.node.nodes[i].key+"-li");
                 li.innerHTML = '<a href="#"><i class="fa fa-microchip"></i>' + response.node.nodes[i].key + '</a>';
                 li.addEventListener ("click", sidebarClickLogic, false);
                 ul.appendChild(li);
@@ -30,7 +35,18 @@ function processRequest(e) {
                     }
                 }
             }
+            //Mark current active Machines with 1
+            sidebarMap.set(response.node.nodes[i].key, 1);
         }
+
+        //Remove old Machines map = 0
+        sidebarMap.forEach(function(value, key, map) {
+            if(value === 0){
+                document.getElementById(key+"-li").remove();
+                document.getElementById(key).remove();
+                sidebarMap.delete(key);
+            }
+        });
     }
 }
 //Event listener to Static pages
