0319ca021c723eb4c482e6a79678ed0b4dd4cc95
Nico Kuhn <kuhn@teco.edu>
2017-11-19 14:16:25 +0100

    Used etcd Data to create dynamic machine iframes and Updated sidebar behaviour to work with undefined numbers of machines

--- dashboard/wwwroot/js/private/etcdLogic.js
+++ dashboard/wwwroot/js/private/etcdLogic.js
@@ -1,12 +1,50 @@
-//curl http://127.0.0.1:2379/v2/keys/Apps
+//Gets etcd information
 var xhr = new XMLHttpRequest();
-xhr.open('GET', "http://192.168.0.29:2379/v2/keys/Apps", true);
-xhr.send();
+xhr.open('GET', "http://192.168.0.29:2379/v2/keys?recursive=true", true); xhr.send(); 
+setInterval(function(){ xhr.open('GET', "http://192.168.0.29:2379/v2/keys?recursive=true", true); xhr.send(); }, 3000);
 xhr.onreadystatechange = processRequest;
 
+//Adds Sidebar Elements
+var sidebarMap = new Map();
 function processRequest(e) {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var response = JSON.parse(xhr.responseText);
-        console.log(response);
+        for (i in response.node.nodes)
+        {
+            if(!sidebarMap.has(response.node.nodes[i].key)){
+                sidebarMap.set(response.node.nodes[i].key, 1);
+                //Add Sidebar Element
+                var ul = document.getElementById("sidebar-ul");
+                var li = document.createElement("li");
+                li.innerHTML = '<a href="#"><i class="fa fa-microchip"></i>' + response.node.nodes[i].key + '</a>';
+                li.addEventListener ("click", sidebarClickLogic, false);
+                ul.appendChild(li);
+                //Add Iframe
+                var content = document.getElementById('page-content-wrapper');
+                var div = document.createElement("div");
+                div.setAttribute("id",response.node.nodes[i].key);
+                for(j in response.node.nodes[i].nodes){
+                    if(response.node.nodes[i].nodes[j].key.slice(-3) === "url"){
+                        div.innerHTML ='<iframe frameborder=0 scrolling="no" src="http://'+ response.node.nodes[i].nodes[j].value+'" style="top:0px; left:0px; bottom:0px; right:0px; width:100%; height:90vh; border:none; margin:0; padding:0; overflow:hidden;"></iframe>'
+                        content.appendChild(div);
+                    }
+                }
+            }
+        }
     }
+}
+//Event listener to Static pages
+$(document).ready(function () {
+    document.getElementById("notify").addEventListener ("click", sidebarClickLogic, false);
+    document.getElementById("main").addEventListener ("click", sidebarClickLogic, false);
+});
+
+//Shows selected sidebare page
+function sidebarClickLogic(event) {
+    for(var child in document.getElementById('page-content-wrapper').childNodes) {
+        if(document.getElementById('page-content-wrapper').childNodes[child].nodeType == 1) {
+            document.getElementById('page-content-wrapper').childNodes[child].style.display = "none";
+        }
+    }
+    document.getElementById(event.path[0].innerText).style.display = "inherit";
 }
